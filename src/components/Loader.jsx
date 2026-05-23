0754fe6218652102a8c2a8c5b6813a89acdc1ddd
MatrixTM26 <anonymous999tm@gmail.com>
2026-05-23 15:54:51 +0700

    My Portfolio

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
-import "../styles/Loader.css";
+import { useEffect, useState } from 'react'
+import '../styles/Loader.css'
 
 export default function Loader() {
-    const [hidden, setHidden] = useState(false);
+  const [hidden, setHidden] = useState(false)
 
-    useEffect(() => {
-        const t = setTimeout(() => setHidden(true), 2200);
-        return () => clearTimeout(t);
-    }, []);
+  useEffect(() => {
+    const t = setTimeout(() => setHidden(true), 2200)
+    return () => clearTimeout(t)
+  }, [])
 
-    return (
-        <div className={`loader-overlay${hidden ? " hidden" : ""}`}>
-            <div className="loader-body">
-                <img src="/loading.svg" alt="Loading" className="loader-svg" />
-                <div className="loader-name">
-                    Matrix<span>TM26</span>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={`loader-overlay${hidden ? ' hidden' : ''}`}>
+      <div className="loader-body">
+        <img src="/loading.svg" alt="Loading" className="loader-svg" />
+        <div className="loader-name">Matrix<span>TM26</span></div>
+      </div>
+    </div>
+  )
 }
